a0faf767b931e91327c5ee22bd755e605db1c75b
scdgogogo <scdgogogo@163.com>
2022-01-11 15:19:15 +0800

    easygitclone 初始化

---
+++ easygitclone/src/main/java/com/tool/easygitclone/EasygitcloneApplication.java
@@ -0,0 +1,13 @@
+package com.tool.easygitclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasygitcloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasygitcloneApplication.class, args);
+	}
+
+}
---
+++ easygitclone/src/test/java/com/tool/easygitclone/EasygitcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tool.easygitclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasygitcloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
